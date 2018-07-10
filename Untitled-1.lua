7b53fe5d766f800976b812f3e25368f38745aea7
Alex <maslovskialexey@mail.ru>
2018-07-10 13:09:35 +0300

    asdsd

--- Untitled-1.lua
+++ Untitled-1.lua
@@ -1,8 +1,8 @@
-triggerBlockIndex = 16
+trigers = { left = 14, right = 15, finish = 16 }
 
 function getSlotIndexWithFood()
     for i=1,16 do
-        if (i ~= triggerBlockIndex and turtle.getItemCount(i) > 0) then 
+        if ((i ~= trigers.left and i ~= trigers.right and i ~= trigers.finish) and turtle.getItemCount(i) > 0) then 
             return i;
         end
     end
@@ -15,8 +15,19 @@ function rotate90right()
     turtle.turnRight()
 end
 
+function rotate90left()
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
 function move()
     turtle.forward()
+end
+
+function farm()
+    turtle.dig()
+    turtle.select(getSlotIndexWithFood())
+    turtle.place()
 end
 
 function oneStep()
@@ -24,9 +35,7 @@ function oneStep()
 
     rotate90right()
 
-    turtle.dig()
-    turtle.select(getSlotIndexWithFood())
-    turtle.place()
+    farm()
 
     rotate90right()
 end
@@ -36,14 +45,67 @@ function canMove()
     return not turtle.compare()
 end
 
+function turnRightIfNeed()
+    turtle.select(trigers.right)
+    
+    if (not turtle.compare()) then
+        return false
+    end
+
+    turtle.turnRight()
+    move()
+
+    rotate90right()
+
+    farm()
+
+    turtle.turnLeft()
+
+    return true 
+end
+
+function turnLeftIfNeed()
+    turtle.select(trigers.left)
+    
+    if (not turtle.compare()) then
+        return false
+    end
+
+    turtle.turnLeft()
+    move()
+
+    rotate90right()
+
+    farm()
+
+    turtle.turnRight()
+
+    return true 
+end
+
+function finish()
+    turtle.select(trigers.finish)
+
+    result = turtle.compare()
+
+    if (result) then 
+        rotate90left()
+    end
+
+    return result
+end
+
 move()
 
 while true do
-    if (not canMove()) then
-        return
-    end
+    turnRightIfNeed()
+    turnLeftIfNeed()
 
     oneStep()
+
+    if (finish()) then
+        return
+    end
 end
 
 
