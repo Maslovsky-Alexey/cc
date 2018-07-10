ef83c968bc55fc7f9a8c8139f7490b5816e8df33
Alex <maslovskialexey@mail.ru>
2018-07-10 19:42:40 +0300

    fix

--- farm.lua
+++ farm.lua
@@ -1,6 +1,6 @@
 local args = { ... }
 
-tasks = (#args > 0 and args[1] = '-r') ? { 'plant', 'harvest' } : { 'harvest', 'plant' } 
+tasks = (#args > 0 and args[1] ~= '-r') ? { 'plant', 'harvest' } : { 'harvest', 'plant' } 
 
 while true do
   os.run(tasks[1])
