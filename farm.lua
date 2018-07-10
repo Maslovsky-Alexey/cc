6160074743d49e4c10df2a81daf2731a2986f959
Alex <maslovskialexey@mail.ru>
2018-07-10 19:22:06 +0300

    farm added

---
+++ farm.lua
@@ -0,0 +1,11 @@
+local args = { ... }
+
+tasks = (#args > 0 and args[1] = '-r') ? { 'plant', 'harvest' } : { 'harvest', 'plant' } 
+
+while true do
+  os.run(tasks[1])
+
+  sleep(60 * 10)
+
+  os.run(tasks[2])
+end
