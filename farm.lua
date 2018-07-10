c2faf9268ae403d6f5bd62360fdaa6e2e35940fc
Alex <maslovskialexey@mail.ru>
2018-07-10 19:51:47 +0300

    asd

--- farm.lua
+++ farm.lua
@@ -3,9 +3,9 @@ local args = { ... }
 tasks = (#args > 0 and args[1] == '-r') ? { 'harvest', 'plant' } : { 'plant', 'harvest' } 
 
 while true do
-  os.run(tasks[1])
+  os.run({14, 15, 16, 13}, tasks[1])
 
   sleep(60 * 10)
 
-  os.run(tasks[2])
+  os.run({14, 15, 16, 13}, tasks[2])
 end
