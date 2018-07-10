7911caff534a84e882ca929f558e58bf2c48c9e4
Alex <maslovskialexey@mail.ru>
2018-07-10 20:06:11 +0300

    asd

--- farm.lua
+++ farm.lua
@@ -7,9 +7,9 @@ if (#args > 0 and args[1] == '-r') then
 end
 
 while true do
-  os.run({14, 15, 16, 13}, tasks[1])
+  shell.run(tasks[1], '14', '15', '16', '13')
 
   sleep(60 * 10)
 
-  os.run({14, 15, 16, 13}, tasks[2])
+  shell.run(tasks[2], '14', '15', '16', '13')
 end
